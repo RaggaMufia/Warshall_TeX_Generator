1c1dce3e144e581ec53e457c02d0665f8b526e31
Joseph LaFreniere <lafrenierejm@gmail.com>
2015-03-31 00:38:24 -0500

    Began working on file input.

--- Warshall.c
+++ Warshall.c
@@ -1,33 +1,66 @@
 #include<stdio.h>	// For terminal io
 #include<stdlib.h>
-#include<stdbool.h>	// For boolean
 
-void warshall(bool matrix[][], int dimension);	// Gets transitive closure of matrix
+int readMatrix(char** matrix, char* inputFileName);	// Creates matrix from file contents, retuns dimensions
+void warshall(char** matrix, int dimension);		// Gets transitive closure of matrix
+void printMatrix(char** matrix, int dimension, FILE* outputFile);	// Output matrix to file
 
 int main() {
-	int n;			// Dimension of square matrix
-	bool matrix[n][n];	// n x n matrix
+	int dimensions;	// Dimension of square matrix
+	char **matrix;	// n x n matrix
+	char[] inputFileName = "input.txt";	// Name of input file
+	char[] outputFileName = "output.tex";	// Name of output file
+
+	FILE *inputFile = fopen("input.txt", "r");	// Input file
+	FILE *outputFile = fopen("output.tex", "w");	// Output file
+
+	/* allocate the array */
+	matrix = malloc(n * sizeof *matrix);
+	for (i = 0; i < n; i++)
+		matrix[i] = malloc(n * sizeof *matrix[i]);
+	
+	for(i = 0; i < n; i++) {
+		for(j = 0; j < n; j++) {
+			matrix[i][j] = '0';
+		}
+	}
 
 	return(EXIT_SUCCESS);
 }
 
-void warshall(bool matrix[][], int dimension) {
+int readMatrix(char** matrix, char* inputFileName) {
+	FILE* inputFile = fopen(inputFileName, "r");	// Opens file in read mode
+}
+
+void warshall(char** matrix, int dimension) {
 	int k, i, j;
 
-	for(k = 0; k <= dimension; k++) {
-		for(i = 0; i <= dimension; i++) {
-			for(j = 0; j <= dimension; j++) {
-				if(matrix[i][j] == true)
-					// Element is already true
-				else if(matrix[i][k] == true && matrix[k][j] == true)
-					matrix[i][j] = true;
+	for(k = 0; k < dimension; k++) {
+		for(i = 0; i < dimension; i++) {
+			for(j = 0; j < dimension; j++) {
+				if(matrix[i][j] == '1') {
+				}
+				else if(matrix[i][k] == '1' && matrix[k][j] == '1')
+					matrix[i][j] = '1';
 			}
 		}
 	}
 	return;	// End returning void
 }
 
-void printMatrix(bool matrix[][], int dimension) {
+void printMatrix(char** matrix, int dimension, FILE* outputFile) {
+	int i, j;
+
+	fprintf(outputFile, "\\begin{bmatrix}");
+	for(i = 0; i < dimension; i++) {
+		fprintf(outputFile, "\n");
+		for(j = 0; j < dimension; j++) {
+			if(j == dimension - 1)	// End of row
+				fprintf(outputFile, "%c\\\\", matrix[i][j]);
+			else
+				fprintf(outputFile, "%c&\t", matrix[i][j]);
+		}
+	}
 
 	return;	// Return void
 }
