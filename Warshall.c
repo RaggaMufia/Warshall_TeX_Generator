4c00fe7a9b828a88ca27acaaafb85204a67a4f75
Joseph LaFreniere <lafrenierejm@gmail.com>
2015-03-31 01:26:20 -0500

    File input is working.

--- Warshall.c
+++ Warshall.c
@@ -3,13 +3,16 @@
 
 int readMatrix(char** matrix, char* inputFileName);	// Creates matrix from file contents, retuns dimensions
 void warshall(char** matrix, int dimension);		// Gets transitive closure of matrix
-void printMatrix(char** matrix, int dimension, FILE* outputFile);	// Output matrix to file
+void printMatrix(char** matrix, int dimension, char* outputFileName);	// Output matrix to file
 
 int main() {
 	int dimensions;	// Dimension of square matrix
 	char **matrix;	// n x n matrix
 	char inputFileName[] = "input.txt";	// Name of input file
 	char outputFileName[] = "output.tex";	// Name of output file
+
+	dimensions = readMatrix(matrix, inputFileName);
+//	printMatrix(matrix, dimensions, outputFileName);
 
 /*	
 	matrix = malloc(n * sizeof *matrix);
@@ -27,17 +30,17 @@ int main() {
 
 int readMatrix(char** matrix, char* inputFileName) {
 	FILE* file = fopen(inputFileName, "r");	// Opens file in read mode
-	int dimension;	// n in nxn matrix
+	int dimension;	// Order
 	int i, j;	// column and row
 	
 	// Check for errors opening file
 	if(!file) {
-		printf("Error! Could not open input file.");
+		printf("Error! Could not open input file.\n");
 		exit(-1);	// Exit with error code
 	}
 	
-	fscanf(file, "%i", dimension);	// Read in height, width
-	matrix = malloc(dimension * sizeof *matrix);
+	fscanf(file, "%i", &dimension);	// Read in height, width
+/*	matrix = malloc(dimension * sizeof *matrix);
 	for(i = 0; i < dimension; i++)
 		matrix[i] = malloc(dimension * sizeof *matrix[i]);
 	for(i = 0; i < dimension; i++) {
@@ -45,14 +48,17 @@ int readMatrix(char** matrix, char* inputFileName) {
 			matrix[i][j] = '0';
 	}
 
-	while(file) 
+	while(file)  {
 		// Reset variables
 		i = 0;
 		j = 0;
 		// Read new coordinates from file
 		fscanf(file, "%i %i", i, j);
 		matrix[i-1][j-1] = '1';
-	
+	}
+*/
+	dimension = 0;
+	return dimension;	// Return order of matrix 
 }
 
 void warshall(char** matrix, int dimension) {
@@ -68,20 +74,27 @@ void warshall(char** matrix, int dimension) {
 			}
 		}
 	}
-	return;	// End returning void
+	return;	// Return void
 }
 
-void printMatrix(char** matrix, int dimension, FILE* outputFile) {
+void printMatrix(char** matrix, int dimension, char* outputFileName) {
 	int i, j;
+	FILE* file = fopen(outputFileName, "w");	// Open output file in write mode
 
-	fprintf(outputFile, "\\begin{bmatrix}");
+	// Check for successful open/creation
+	if(!file) {
+		printf("Error! Could not open outputFile.\n");
+		exit(-2);
+	}
+
+	fprintf(file, "\\begin{bmatrix}");
 	for(i = 0; i < dimension; i++) {
-		fprintf(outputFile, "\n");
+		fprintf(file, "\n");
 		for(j = 0; j < dimension; j++) {
 			if(j == dimension - 1)	// End of row
-				fprintf(outputFile, "%c\\\\", matrix[i][j]);
+				fprintf(file, "%c\\\\", matrix[i][j]);
 			else
-				fprintf(outputFile, "%c&\t", matrix[i][j]);
+				fprintf(file, "%c&\t", matrix[i][j]);
 		}
 	}
 
